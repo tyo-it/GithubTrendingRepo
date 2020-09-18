be70b2df43fddf868ffd890626e56b00cf66f46a
tyo-it <istantyo.0@gmail.com>
2020-09-18 10:22:08 +0700

    fix: add recycler view divider

--- app/src/main/java/com/ittyo/githubtrendingrepo/MainActivity.kt
+++ app/src/main/java/com/ittyo/githubtrendingrepo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ittyo.githubtrendingrepo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ittyo.githubtrendingrepo.repository.TrendingRepoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,11 +17,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+
         repo_recycler_view.layoutManager = LinearLayoutManager(this)
+        repo_recycler_view.addItemDecoration(divider)
         repo_recycler_view.adapter = repoAdapter
-        repo_recycler_view.visibility = View.GONE
 
         loading_recycler_view.layoutManager = LinearLayoutManager(this)
+        loading_recycler_view.addItemDecoration(divider)
         loading_recycler_view.adapter = loadingAdapter
+        loading_recycler_view.visibility = View.GONE
     }
 }
