a44256960c67622f4bdd5de9c0ac5fbbfa3242d6
tyo-it <istantyo.0@gmail.com>
2020-09-19 07:44:04 +0700

    fix: failing unit test

--- app/src/test/java/com/ittyo/githubtrendingrepo/Helper.kt
+++ app/src/test/java/com/ittyo/githubtrendingrepo/Helper.kt
@@ -6,8 +6,9 @@ fun createRepo(
     author: String = "author",
     name: String = "name",
     avatarUrl: String = "avatarUrl",
+    repositoryUrl: String = "repositoryUrl",
     description: String = "description",
     language: String = "language",
     stars: Int = 0,
     forks: Int = 0
-) = Repo(author, name, avatarUrl, description, language, stars, forks)
+) = Repo(author, name, avatarUrl, repositoryUrl, description, language, stars, forks)
